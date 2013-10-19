4f829674d31e78bb1ce32503c93befd73f03d7c5
Marko Nikolic <nikolic89@hotmail.com>
2013-10-19 18:22:30 +0200

    Update used state implemented.

--- src/Acme/DashboardBundle/Controller/ApiController.php
+++ src/Acme/DashboardBundle/Controller/ApiController.php
@@ -20,10 +20,39 @@ class ApiController extends Controller
  	public function updateQrcodeAction($secret)
     {
 
+    	$response = array();
 
+	    $em = $this->getDoctrine()->getManager();
+	    $code = $em->getRepository('AcmeDashboardBundle:Qrcode')->findOneBy(array( 'secret' => $secret));
 
-    	
-    	$response = array('success' => true, 'secret' => $secret);
+	    if (!$code) {
+	       $response = array('success' => false,
+ 				  'secret' => $secret,
+ 				  'code' => null,
+ 				  'error' => 'No code found for secret = '.$secret 
+ 			);
+	    }
+	    else
+	    {
+	    	$code->setUsed(true);
+	    	$code->setUpdated(new \DateTime("now"));
+	    	$em->flush();
+
+	    	$response = array('success' => true,
+ 				  'secret' => $secret,
+ 				  'code'=> array('id' => $code->getId(),
+ 				  				 'weight' =>  $code->getWeight(),
+ 				  				 'used' => $code->getUsed(),
+ 				  				 'secret' => $code->getSecret(),
+ 				  				 'created' => $code->getCreated()->format('Y-m-d H:i:s'),
+ 				  				 'updated' => $code->getUpdated()->format('Y-m-d H:i:s'),
+ 				  				 'filename' => $code->getFilename(),
+ 				  				 'user' => $code->getUser()
+ 				  				 ),
+ 				  'error' => null
+ 				);
+
+	    }
 
   		return new Response(json_encode($response)); 
     }  
