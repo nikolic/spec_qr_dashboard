3577e0380c73d0598657fb827c0e15193639f80c
Marko Nikolic <nikolic89@hotmail.com>
2013-10-26 16:49:08 +0200

    Show action added in Api controller

--- src/Acme/DashboardBundle/Controller/ApiController.php
+++ src/Acme/DashboardBundle/Controller/ApiController.php
@@ -17,6 +17,44 @@ class ApiController extends Controller
   		return new Response(json_encode($response)); 
     }  
 
+ 	public function showQrcodeAction($secret)
+    {
+
+    	$response = array();
+
+	    $em = $this->getDoctrine()->getManager();
+	    $code = $em->getRepository('AcmeDashboardBundle:Qrcode')->findOneBy(array( 'secret' => $secret));
+
+	    if (!$code) {
+	       $response = array('success' => false,
+ 				  'secret' => $secret,
+ 				  'code' => null,
+ 				  'image_path' => null,
+ 				  'error' => 'No code found for secret = '.$secret 
+ 			);
+	    }
+	    elseif($code->getUsed()){
+	    	$response = array('success' => false,
+ 				  'secret' => $secret,
+ 				  'code' => $code->toArray(),
+ 				  'image_path' => "http://localhost:8000/codes/" . $code->getFilename(),
+ 				  'error' => 'Already used.' 
+ 			);	
+	    }
+	    else
+	    {
+
+	    	$response = array('success' => true,
+ 				  'secret' => $secret,
+ 				  'code'=> $code->toArray(),
+ 				  'image_path' => "http://localhost:8000/codes/" . $code->getFilename(),
+ 				  'error' => null
+ 				);
+	    }
+
+  		return new Response(json_encode($response)); 
+    }  
+
  	public function updateQrcodeAction($secret)
     {
 
@@ -54,6 +92,6 @@ class ApiController extends Controller
 	    }
 
   		return new Response(json_encode($response)); 
-    }  
+    }
 
 }
